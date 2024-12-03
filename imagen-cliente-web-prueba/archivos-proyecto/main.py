bb796c4b38a227bc327f3615dbe50bef3ba457dc
rafa1485 <rafaelvargas.obera@gmail.com>
2024-12-03 01:14:18 -0300

    agregamos la aplicacion de seleccion optima de mezcla

--- imagen-cliente-web-prueba/archivos-proyecto/main.py
+++ imagen-cliente-web-prueba/archivos-proyecto/main.py
@@ -396,14 +396,231 @@ def descargar_resultados_manual():
     except Exception as e:
         return f"Error al descargar el archivo: {str(e)}", 500
 
+
+#---------------------------------------------------------------------------------
 # Aplicacion de mezcla óptima
-@app.route('/mezcla_optima')
+
+# Ruta para seleccionar ingredientes y calcular los valores
+@app.route('/mezcla_optima', methods=['GET', 'POST'])
 @login_required
 def mezcla_optima():
-    return f'''
-               <h1>Bienvenido a la prueba de Mezcla Óptima, {current_user.nombre}!</h1>
-               <a href='/logout'>Cerrar sesión</a>
-            '''
+    #ingredientes = []
+    score_proteico = 0
+    costo_por_kg = 0
+    porcentaje_total = 0
+
+    # Muestra todos los ingredientes para seleccionar
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute("SELECT id, nombre FROM ingredientes")
+    ingredientes = cursor.fetchall()
+
+    cursor.execute("SELECT id, digestibilidad_proteica FROM ingredientes")
+    digestibilidades = cursor.fetchall()
+    
+    conexion.close()
+
+    #dict_id_ingredientes = dict(ingredientes)
+
+    # Creo un diccionario de las digestibilidades indexado por el id
+    dict_id_digestibilidades = dict(digestibilidades)
+
+    if request.method == 'POST':
+
+        porcentajes_min = request.form.getlist('porcentaje_min')
+        print('porcentajes_min')
+        print(porcentajes_min)
+
+        # obtengo los porcentajes minimos en mezcla y creo un diccionario que vincula cada ingrediente
+        # dado por su 'id' con el porcentaje minimo correspondiente
+        porcentajes_num_min = [int(x) if x != '' else 0 for x in porcentajes_min]
+        lista_ingredientes = [id for id,_ in ingredientes]
+        dict_id_porcentajes_min = dict(zip(lista_ingredientes,porcentajes_num_min))
+        print(dict_id_porcentajes_min)
+
+        porcentajes_max = request.form.getlist('porcentaje_max')
+        print('porcentajes_max')
+        print(porcentajes_max)
+
+        # obtengo los porcentajes minimos en mezcla y creo un diccionario que vincula cada ingrediente
+        # dado por su 'id' con el porcentaje minimo correspondiente
+        porcentajes_num_max = [int(x) if x != '' else 0 for x in porcentajes_max]
+        lista_ingredientes = [id for id,_ in ingredientes]
+        dict_id_porcentajes_max = dict(zip(lista_ingredientes,porcentajes_num_max))
+        print(dict_id_porcentajes_max)
+
+        id_ingredientes_seleccionados = []
+        for id in lista_ingredientes:
+            if (dict_id_porcentajes_max[id] > 0) and (dict_id_porcentajes_max[id] > dict_id_porcentajes_min[id]):
+                id_ingredientes_seleccionados.append(id)
+        
+        if len(id_ingredientes_seleccionados) != 0:
+        
+
+            print('ingredientes seleccionados')
+            print(id_ingredientes_seleccionados)
+
+            # Calculamos el porcentaje total de los ingredientes.
+            # En caso que los ingredientes sumen un valor distinto de 100% se debe imprimir un alerta.
+            porcentaje_total_min = sum(porcentajes_num_min)
+            porcentaje_total_max = sum(porcentajes_num_max)
+            if porcentaje_total_max < 100:
+                flash("Porcentaje total máximos menor a 100%: Aumentar los porcentajes maximos de los ingredientes", "info")
+            elif porcentaje_total_min > 100:
+                flash("Porcentaje total mínimo mayor a 100%: Disminuir los porcentajes mínimos de los ingredientes", "info")
+            
+            # inicializo los diccionarios necesarios para hacer las cuentas
+            dict_id_nombre = {}
+            dict_id_cont_proteina = {}
+            dict_id_digest_proteina = {}
+            dict_id_precio = {}
+            dict_id_amino = {}
+            dict_id_carbohidr = {}
+            dict_id_lipidos = {}
+
+
+            for id_ingrediente_seleccionado in id_ingredientes_seleccionados:
+                porcentaje_str_min = str(dict_id_porcentajes_min[id_ingrediente_seleccionado])
+                porcentaje_str_max = str(dict_id_porcentajes_max[id_ingrediente_seleccionado])
+                ingrediente_info = obtener_info_ingrediente(id_ingrediente_seleccionado)
+                
+                if ingrediente_info:
+                    print('porcentaje min: '+ porcentaje_str_min)
+                    print('porcentaje max: '+ porcentaje_str_max)
+                    print('ingrediente info: ')
+                    print(ingrediente_info)
+                    
+                    nombre_ingre = ingrediente_info[1]
+
+                    contenido_proteico = float(ingrediente_info[6])
+
+                    digest_proteina = float(ingrediente_info[5])
+
+                    precio = float(ingrediente_info[3])
+
+                    densidad = float(ingrediente_info[2])
+
+                    contenido_carbohidr = float(ingrediente_info[7])
+                    
+                    contenido_lipidos = float(ingrediente_info[8])
+
+                    dict_id_nombre.update({id_ingrediente_seleccionado:nombre_ingre})
+
+                    dict_id_precio.update({id_ingrediente_seleccionado:precio})
+
+                    dict_id_cont_proteina.update({id_ingrediente_seleccionado:contenido_proteico})
+
+                    dict_id_digest_proteina.update({id_ingrediente_seleccionado:digest_proteina})
+
+                    dict_id_amino.update({id_ingrediente_seleccionado:list(ingrediente_info[9:18])})
+
+                    dict_id_carbohidr.update({id_ingrediente_seleccionado:contenido_carbohidr})
+
+                    dict_id_lipidos.update({id_ingrediente_seleccionado:contenido_lipidos})
+
+                    #score_proteico += (contenido_proteico * float(porcentaje_str) / 100)
+                    #costo_por_kg += (precio * float(porcentaje_str) / 100)
+                else:
+                    print('El ingrediente '+id_ingrediente_seleccionado+' no se encuentra en la base de datos.')
+                    breakpoint()
+            #print(dict_id_precio)
+            #print(dict_id_cont_proteina)
+            #print(contenido_carbohidr)
+            #print(contenido_lipidos)
+            #print(dict_id_amino)
+            
+            
+            D_i = [] # Vector Digestibilidad de la proteína de cada ingrediente
+            W_i = [] # Vector Porcentajes de Ingrediente
+            Costo_i = [] # Vector Costo de ingredientes
+            P_i = [] # Vector Composición Proteica de Ingredientes por gr de ingrediente
+            AA_ij = [] # Matriz del j-esimo Amino Ácidos del ingrediente i
+            
+
+            for id in id_ingredientes_seleccionados:
+                D_i.append([dict_id_digest_proteina[id]])
+
+                #W_i.append([dict_id_porcentajes[id]])
+
+                Costo_i.append([dict_id_precio[id]])
+                P_i.append([dict_id_cont_proteina[id]])
+                AA_ij.append(dict_id_amino[id])
+
+            
+            req_AA = [[requerimiento_aminoacidos_esenciales[j] for j in requerimiento_aminoacidos_esenciales.keys()]] # Requerimientos de amonoacidos esenciales
+            
+            D = np.array(D_i)
+            #W = np.array(W_i)/100
+            C = np.array(Costo_i)
+            P = np.array(P_i)/100
+            AA = np.array(AA_ij)
+            rAA = np.array(req_AA)
+            
+
+            #---------------------------------------------------------------------------------------
+            # Calculo del costo de la materia prima
+            #costo_por_kg = np.dot(W.transpose(),C)[0,0]
+
+            #----------------------------------------------------
+            # Contenido proteico
+            print(P)
+
+            #----------------------------------------------------
+            # Fracción de cada proteína presente en la mezcla
+            #WP = W*P
+            #print(WP)
+            print('Proteína total en un gr de mezcla')
+            #P_mezcla = WP.sum()
+            P_mezcla = 0
+            print(P_mezcla)
+
+            #---------------------------------------------------------------------------------------
+            # Calculo de la digestigilidad promedio de la mezcla
+            #Dm = (np.dot(WP.transpose(),D)/P_mezcla)[0,0]
+            print('Digestibilidad promedio')
+            #print(Dm)
+
+            #--------------------------------------------------------------------------------------
+            # mg del j-esimo Aminoacido esencial para cada gramo del ingrediente i
+            print('mg de Aminoácido por cada gr de ingredietne i')
+            print(AA)
+            
+            #--------------------------------------------------------------------------------------
+            # mg del j-esimo Aminoacido esencial en cada gramo de mezcla
+            print('mgr de Aminoacidos por gr de mezcla')
+            #AA_mezcla = np.dot(WP.transpose(),AA)
+            #print(AA_mezcla)
+
+            #--------------------------------------------------------------------------------------
+            # calculo de los ASS_j
+            #AAS = np.divide((AA_mezcla / P_mezcla) , rAA)
+            print('Puntuación de Aminoácidos (AAS)')
+            #print(AAS)
+
+            #PDCAAS = AAS.min() * Dm
+
+            #wb = crear_tabla_calculos(nombres=dict_id_nombre, fraccion_proteina=dict_id_cont_proteina, digestibilidad_proteina=dict_id_digest_proteina, contenido_aminoacidos=dict_id_amino, requerimientos=req_AA, porcentajes_mezcla=W, aminoacidos_mezcla_gr_mezcla=AA_mezcla, fraccion_proteina_mezcla=P_mezcla ,puntaje_aminoacidos=AAS, digestibilidad=Dm, PDCAAS=PDCAAS)
+
+            #wb.save('./resultados_calculos_ejemplo.xlsx')
+
+            #score_proteico = round(PDCAAS,5)
+
+            #costo_kg_prot_asimilable = round(costo_por_kg/(score_proteico*P_mezcla), 2)
+            P_mezcla = 0
+            costo_kg_prot_asimilable = 0
+            dict_id_porcentajes = dict_id_porcentajes_max
+
+            return render_template('mezcla_optima.html', ingredientes=ingredientes, digestibilidades=dict_id_digestibilidades, porcentajes_num=dict_id_porcentajes,
+                                aminoacidos=aminoacidos ,referencia_aminoacidos=requerimiento_aminoacidos_esenciales,
+                                score_proteico=score_proteico, costo_por_kg=costo_por_kg, fraccion_proteina=P_mezcla,
+                                porcentaje_total=porcentaje_total, costo_kg_prot_asimilable=costo_kg_prot_asimilable)
+
+    
+    return render_template('mezcla_optima.html', ingredientes=ingredientes, digestibilidades=dict_id_digestibilidades, porcentajes_num=False,
+                            aminoacidos=aminoacidos ,referencia_aminoacidos=requerimiento_aminoacidos_esenciales,
+                            score_proteico=score_proteico, costo_por_kg=costo_por_kg, fraccion_proteina=None, 
+                            porcentaje_total=porcentaje_total, costo_kg_prot_asimilable=None)
 
 
 
